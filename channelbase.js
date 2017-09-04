60cc17a2124801acb1325233d04dbff3bfac13a6
Mickey Malone <SuperScoops@users.noreply.github.com>
2017-09-04 19:53:26 +1000

    Update channelbase.js

--- channelbase.js
+++ channelbase.js
@@ -91,7 +91,7 @@ $('#queuecontainer').hover(function(){
         'opacity':'0',
     });
 });
-$("#wrap").prepend("<div id='bg-wrapper'><div id='backg'></div></div>");
+//$("#wrap").prepend("<div id='bg-wrapper'><div id='backg'></div></div>");
 $("body").addClass("fluid");
 $("#videowrap").addClass("col-lg-7 col-md-7 vjs-polyzor-skin");
 $("#videowrap").removeClass("col-md-8 col-md-offset-2");

80306ea48d752467645a34aa541f26a598d56e64
Mickey Malone <SuperScoops@users.noreply.github.com>
2017-09-04 21:26:04 +1000

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -675,7 +675,7 @@ var LOADED = (typeof LOADED==="undefined") ? false : true;
 LOADED ? location.reload() : '';
 
 
-var bgColorArray = ['//i.imgur.com/VQ5yitw.gif'],
+var bgColorArray = ['//i.imgur.com/dPrY8gp.jpg,//i.imgur.com/xoI7bpp.png,//i.imgur.com/AOCLnfM.jpg,//i.imgur.com/zJ73fiT.png,//i.imgur.com/ckGvKW5.png,//i.imgur.com/RbP0rDe.gif,i.imgur.com/ksHsMC8.png'],
 
 selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 $('#backg').css('background', 'url(' + selectBG + ')')
